1681c5f1cd07ed52256b9ee5854c54b6e87b0b88
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-11-21 13:21:10 +0200

    create the network infra for the EC2 instance to run Grafana

--- main.tf
+++ main.tf
@@ -1,13 +1,86 @@
-# Add your code here: 
+resource "aws_subnet" "subnet"{
+  vpc_id     = var.vpc_id
+  cidr_block = "10.0.1.0/24"
 
-# 1. Create a subnet 
-# 2. Create an Internet Gateway and attach it to the vpc
-# 3. Configure routing for the Internet Gateway
-# 4. Create a Security Group and inbound rules 
-# 5. Uncommend (and update the value of security_group_id if required) outbound rule - it required 
-# to allow outbound traffic from your virtual machine: 
-# resource "aws_vpc_security_group_egress_rule" "allow_all_eggress" {
-#   security_group_id = aws_security_group.security_group.id
-#   cidr_ipv4   = "0.0.0.0/0"
-#   ip_protocol = -1
-# }
+  tags = {
+    Name = "grafana"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "security_group" {
+  name        = "mate-aws-grafana-lab"
+  description = "Security group for Grafana lab"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.security_group.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.security_group.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.security_group.id
+
+  cidr_ipv4   = "193.194.126.182/32"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_grafana" {
+  security_group_id = aws_security_group.security_group.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_eggress" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,1 @@
-vpc_id = ""
+vpc_id = "vpc-01f0136f02a1adb40"
